ddc7e0e4ccda86a5b1e553a3e12fda9fcc9820d7
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-09-11 19:16:46 -0300

    swiper funcionando en providers index

--- resources/views/provider/index.blade.php
+++ resources/views/provider/index.blade.php
@@ -7,39 +7,55 @@
 
 @section('contenido')
 
-    <div class="provider__contenedor-boton" >
+    <div class="provider__contenedor-boton">
         <a href="{{ route('provider.create') }}" class="provider__boton">Crear Proveedor</a>
         <a href="#" class="provider__boton">Buscar Proveedor</a>
     </div>
 
 
+
+
+
     @if ($providers->count() > 0)
-        <div class=" provider__grid">
-            @foreach ($providers as $provider)
-                <div class=" provider__contenedor">
-                    <p><span class=" font-bold">provider: </span>{{$provider->nombre}}</p>
-                    <p><span class=" font-bold">Email: </span>{{$provider->email}}</p>
-                    <p><span class=" font-bold">Telefono: </span>{{$provider->telefono}}</p>
-                    <p><span class=" font-bold">Vendedor: </span>{{$provider->vendedor}}</p>
-                    <p><span class=" font-bold">Web: </span>{{$provider->web}}</p>
-                    <p><span class=" font-bold">Ganancia: </span>{{$provider->ganancia}}</p>
+        <div class="swiper slider"> <!-- Swiper principal -->
+            <div class="swiper-wrapper"> <!-- Swiper secundario -->
 
-                    <div class="provider__contenedor-boton provider__contenedor-boton--sm">
-                        <a class="provider__boton provider__boton--modificar" href="{{ route('provider.edit', $provider) }}">Ver / Editar</a>
-                        <form action="{{ route('provider.destroy', $provider) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="provider__boton provider__boton--eliminar">Eliminar</button>
-                        </form>
+
+
+                @foreach ($providers as $provider)
+                    <div class=" provider__contenedor swiper-slide">
+                        <h3>{{ $provider->nombre }}</h3>
+                        <p><span class="font-bold">Ganancia: </span><span class="provider__ganancia">{{ $provider->ganancia }}</span></p>
+                        <p><span class=" font-bold">Email: </span>{{ $provider->email }}</p>
+                        <p><span class=" font-bold">Telefono: </span>{{ $provider->telefono }}</p>
+                        <p><span class=" font-bold">Vendedor: </span>{{ $provider->vendedor }}</p>
+                        <p><span class=" font-bold">Web: </span>{{ $provider->web }}</p>
+
+                        <div class="provider__contenedor-boton provider__contenedor-boton--sm">
+                            <a class="provider__boton provider__boton--modificar"
+                                href="{{ route('provider.edit', $provider) }}">Ver / Editar</a>
+                            <form action="{{ route('provider.destroy', $provider) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="provider__boton provider__boton--eliminar">Eliminar</button>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            @endforeach
-        </div>
+                @endforeach
 
-        <div class="" >
-            {{ $providers->links() }}
-        </div>
+            </div> <!-- Swiper secundario -->
 
+            
+            <div class="swiper-pagination"></div> <!-- Pagination -->
+
+            <!-- Navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+
+            
+
+
+        </div> <!-- Swiper principal -->
     @else
         <p class="mensaje__vacio">No se encontraron proveedores</p>
     @endif
